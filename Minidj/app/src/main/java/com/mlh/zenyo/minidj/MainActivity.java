cfe44e201de47e4ef46b478ee5eabf2ec80c8e63
now <now@googlemail.de>
2016-12-03 14:42:56 +0100

    Layout

--- Minidj/app/src/main/java/com/mlh/zenyo/minidj/MainActivity.java
+++ Minidj/app/src/main/java/com/mlh/zenyo/minidj/MainActivity.java
@@ -2,14 +2,24 @@ package com.mlh.zenyo.minidj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.bPlay1);
+        b1.setOnClickListener(handlerPlay1);
     }
+    View.OnClickListener handlerPlay1 = new View.OnClickListener() {
+        public void onClick(View v) {
+            // TCP Connector Play
+        }
+    };
 }
