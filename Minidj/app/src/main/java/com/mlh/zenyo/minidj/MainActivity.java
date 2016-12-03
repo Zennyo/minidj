c5dcaa3762aa18e7bd778c18b8bcfc5dc0e4e587
now <now@googlemail.de>
2016-12-03 17:24:53 +0100

    Handler add

--- Minidj/app/src/main/java/com/mlh/zenyo/minidj/MainActivity.java
+++ Minidj/app/src/main/java/com/mlh/zenyo/minidj/MainActivity.java
@@ -2,14 +2,17 @@ package com.mlh.zenyo.minidj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PORT = 6080;
+    private static final String IP_ADDRESS = "172.22.211.168";
     private ImageButton bplay1;
     private ImageButton bplay2;
     private ImageButton bstop1;
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private Button bf3;
     private Button bf4;
     private Button bf5;
+    private SeekBar sb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,72 +55,88 @@ public class MainActivity extends AppCompatActivity {
         bf4.setOnClickListener(handlerF4);
         bf5 = (Button) findViewById(R.id.bf5);
         bf5.setOnClickListener(handlerF5);
+
+        sb = (SeekBar) findViewById(R.id.seekBar);
+        sb.setOnTouchListener(handlerSeekbar);
     }
     View.OnClickListener handlerPlay1 = new View.OnClickListener() {
         public void onClick(View v) {
             // TCP Connector Play
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Play1", Toast.LENGTH_SHORT).show();
         }
     };
     View.OnClickListener handlerPlay2 = new View.OnClickListener() {
         public void onClick(View v) {
             // TCP Connector Play
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Play2", Toast.LENGTH_SHORT).show();
         }
     };
     View.OnClickListener handlerStop1 = new View.OnClickListener() {
         public void onClick(View v) {
             // TCP Connector
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Stop1", Toast.LENGTH_SHORT).show();
         }
     };
     View.OnClickListener handlerStop2 = new View.OnClickListener() {
         public void onClick(View v) {
             // TCP Connector
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Stop2", Toast.LENGTH_SHORT).show();
         }
     };
     View.OnClickListener handlerPause1 = new View.OnClickListener() {
         public void onClick(View v) {
             // TCP Connector
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Pause1", Toast.LENGTH_SHORT).show();
         }
     };
     View.OnClickListener handlerPause2 = new View.OnClickListener() {
         public void onClick(View v) {
             // TCP Connector
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Pause2", Toast.LENGTH_SHORT).show();
         }
     };
     View.OnClickListener handlerF1 = new View.OnClickListener() {
         public void onClick(View v) {
             // TCP Connector
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "F1", Toast.LENGTH_SHORT).show();
         }
     };
     View.OnClickListener handlerF2 = new View.OnClickListener() {
         public void onClick(View v) {
             // TCP Connector
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "F2", Toast.LENGTH_SHORT).show();
         }
     };
     View.OnClickListener handlerF3 = new View.OnClickListener() {
         public void onClick(View v) {
             // TCP Connector
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "F3", Toast.LENGTH_SHORT).show();
         }
     };
     View.OnClickListener handlerF4 = new View.OnClickListener() {
         public void onClick(View v) {
             // TCP Connector
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "F4", Toast.LENGTH_SHORT).show();
         }
     };
     View.OnClickListener handlerF5 = new View.OnClickListener() {
         public void onClick(View v) {
             // TCP Connector
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "F5", Toast.LENGTH_SHORT).show();
         }
+    };
+    SeekBar.OnTouchListener handlerSeekbar = new SeekBar.OnTouchListener () {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+
+            // TCP Connector
+            Toast.makeText(MainActivity.this, "Seekbar: " + ((int) sb.getProgress()), Toast.LENGTH_SHORT).show();
+            TCPClient tcp = new TCPClient(IP_ADDRESS,PORT, "");
+
+            return false;
+        }
+
+
     };
 
 
